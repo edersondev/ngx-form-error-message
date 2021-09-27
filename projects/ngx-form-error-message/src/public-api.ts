dbdb0b62bbfe9a820110fd7aa8000dc73812d115
Ederson Ferreira <ederson.dev@gmail.com>
2021-09-27 10:30:29 -0300

    update package.json and public-api.ts

--- projects/ngx-form-error-message/src/public-api.ts
+++ projects/ngx-form-error-message/src/public-api.ts
@@ -2,6 +2,5 @@
  * Public API Surface of ngx-form-error-message
  */
 
-export * from './lib/ngx-form-error-message.service';
-export * from './lib/ngx-form-error-message.component';
+export * from './lib/ngx-error-message.directive';
 export * from './lib/ngx-form-error-message.module';
